3235b92bdefe3ecf42e974423ac3db758efc3339
Pid24 <rofidnasifannafie@gmail.com>
2023-07-12 10:03:02 +0700

    slicing product page

--- resources/views/user/productpage.blade.php
+++ resources/views/user/productpage.blade.php
@@ -8,7 +8,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col text-center">
-                <h1 class="text-white fw-bold mb-4">Layanan Kami</h1>
+                <h1 class="text-white fw-bold mb-4">UMKM Kami</h1>
                 <p class="text-white mb-5 text-opacity-75">
                     Berikut adalah layanan yang bisa kami berikan untuk Anda
                 </p>
@@ -17,25 +17,67 @@
     </div>
 </div>
 
-{{-- service --}}
-<div class="service">
+{{-- umkm --}}
+<div class="umkm">
     <div class="container">
         <div class="row">
             <div class="col">
-                <div class=" text-center">
-                    <h3 class="title">Mau Service Apa Hari Ini?</h3>
+                <div class="umkm-button">
+                    <div class="col d-flex justify-content-center align-items-center">                        
+                        <button type="button" class="btn btn-primary mx-3">Makanan</button>
+                        <button type="button" class="btn btn-secondary mx-3">Fashion</button>
+                        <button type="button" class="btn btn-secondary mx-3">Elektronik</button>
+                        <button type="button" class="btn btn-secondary mx-3">Jasa</button>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="row d-flex justify-content-center align-items-center">
-            <div class="col col-lg-8">
-                <form action="" method="GET">
-                    @csrf
-                    <input type="text" class="form-control" placeholder="Cari bengkel disini" name="keyword">
-                </form>
+        <div class="umkm-card d-flex flex-row justify-content-center align-items-center">
+            <div class="card mx-3">
+                <img src="/public/css/work.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mutiara Jaya AC</h5>
+                    <p class="card-text">Tangerang</p>
+                </div>
+            </div>
+            <div class="card mx-3">
+                <img src="/public/css/work.png"" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mutiara Jaya AC</h5>
+                    <p class="card-text">Tangerang</p>
+                </div>
+            </div>
+            <div class="card mx-3">
+                <img src="/public/css/work.png"" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mutiara Jaya AC</h5>
+                    <p class="card-text">Tangerang</p>
+                </div>
             </div>
         </div>
-
+        <div class="umkm-card d-flex flex-row justify-content-center align-items-center">
+            <div class="card mx-3">
+                <img src="/public/css/work.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mutiara Jaya AC</h5>
+                    <p class="card-text">Tangerang</p>
+                </div>
+            </div>
+            <div class="card mx-3">
+                <img src="/public/css/work.png"" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mutiara Jaya AC</h5>
+                    <p class="card-text">Tangerang</p>
+                </div>
+            </div>
+            <div class="card mx-3">
+                <img src="/public/css/work.png"" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mutiara Jaya AC</h5>
+                    <p class="card-text">Tangerang</p>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
